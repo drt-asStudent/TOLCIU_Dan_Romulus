53f69d1abcaf4673860ec8dcfac91f37914f3e26
DanTolciu <58403381+DanTolciu@users.noreply.github.com>
2025-06-14 16:42:17 +0300

    Note: C:\Users\dan_t\IdeaProjects\TOLCIU_Dan_Romulus\src\main\java\org\example\Helper.java uses or overrides a deprecated API.

--- src/main/java/org/example/Helper.java
+++ src/main/java/org/example/Helper.java
@@ -1,12 +1,16 @@
 package org.example;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
 
 public class Helper {
 
     List<Sala> lista = new ArrayList<>();
+    int conv = 0;
 
     public List<Sala> DinFisier(String file) {
 
@@ -25,8 +29,13 @@ public class Helper {
             while ((str = dis.readLine()) != null) {
                 //System.out.println(str);
                 String[] spart = str.split(" ");
-
-                lista.add(new Sala(spart[0], spart[1], spart[2], Integer.parseInt(spart[3])));
+                try {
+                    conv = Integer.parseInt(spart[3]);
+                } catch (Exception e) {
+                }
+                lista.add(new Sala(spart[0], spart[1], spart[2], conv));
+                //lista.add(new Sala(spart[0], spart[1], spart[2], Integer.parseInt(spart[3])));
+                //lista.add(new Sala(spart[0], spart[1], spart[2], Integer.valueOf(spart[3])));
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
